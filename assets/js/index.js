0ef83c504ec4362c8cbb8d4070d4d001c9978365
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-19 12:59:58 +0000

    setup public and private keys

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
 function () => {}
+
+var api = require('marvel-api');
+
+var marvel = api.createClient({
+publicKey: 'd8c5ab67e26fc975e50148f6ca8696aa'
+, privateKey: 'd047b18cb71085142c82cd13ebad23df79df298e'
+});
